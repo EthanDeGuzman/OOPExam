95b80a52fa32d38712399e2701d244c4605c777e
Ethan De Guzman <S00199053@mail.itsligo.ie>
2021-01-11 10:10:32 +0000

    Created CurrentAccount and SavingsAccount inherit classes

--- Account.cs
+++ Account.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace OOPExam
 {
@@ -12,5 +14,33 @@ namespace OOPExam
         public string LastName { get; set; }
         public double Balance { get; set; }
         public string InterestDate { get; set; }
+
+        public abstract double CalculateInterest();
+
+        public override string ToString()
+        {
+            return $"{LastName.ToUpper()}, {FirstName}";
+        }
+
+    }
+
+    public class CurrentAccount : Account
+    {
+        const double InterestRate = 0.03;
+        public override double CalculateInterest()
+        {
+            InterestDate = DateTime.Now.ToString("MM-dd-yyyy");
+            return Balance * InterestRate;
+        }
+    }
+
+    public class SavingsAccount : Account
+    {
+        const double InterestRate = 0.06;
+        public override double CalculateInterest()
+        {
+            InterestDate = DateTime.Now.ToString("MM-dd-yyyy");
+            return Balance * InterestRate;
+        }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,11 +29,16 @@ namespace OOPExam
 
     public partial class MainWindow : Window
     {
+        ObservableCollection<Account> Accounts = new ObservableCollection<Account>();
+        ObservableCollection<Account> filteredAccounts = new ObservableCollection<Account>();
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
     }
 }
